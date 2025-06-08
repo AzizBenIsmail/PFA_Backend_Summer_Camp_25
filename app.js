17bb8aa3314c5330d471bd7993f77d7a2cc4cc88
AzizBenIsmail <aziz.270700@gmail.com>
2025-06-08 10:50:19 +0100

    connect to db

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const http = require("http") //1 import web "http"
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const http = require("http"); //1 import web "http"
+const { connectToMongoDB } = require("./db/db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-require("dotenv").config() // configuration .env
+require("dotenv").config(); // configuration .env
 
 var app = express(); //Bdina
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json('error');
+  res.json("error");
 });
 
-const server = http.createServer(app)
-server.listen(process.env.Port,()=>{console.log("app is running on port 5000")})
+const server = http.createServer(app);
+server.listen(process.env.Port, () => {
+  connectToMongoDB(),
+  console.log("app is running on port 5000");
+});
